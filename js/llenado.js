1e3b4f54e1eba85372aed8768834bf93067c1ae7
Kmilo <bichozx@hotmail.com>
2023-06-11 19:48:16 -0500

    se corrige la imagen de las papas

--- js/llenado.js
+++ js/llenado.js
@@ -14,25 +14,24 @@ fetch('info.json')
 const paintData = (data) => {
     let fila = document.getElementById("fila")
     data.forEach(productos => {
-       
-       
+
         let columna = document.createElement("div")
         columna.classList.add("col")
-    
+
         let tarjeta = document.createElement("div")
         tarjeta.classList.add("card")
         tarjeta.classList.add("p-2")
         tarjeta.classList.add("shadow")
-    
+
         let imageProduct = document.createElement("img")
         imageProduct.classList.add("card-img-top")
         imageProduct.src = productos.image
-    
+
         let divisor = document.createElement("hr")
-    
+
         let cuerpoTarjeta = document.createElement("div")
         cuerpoTarjeta.classList.add("card-body")
-    
+
         let nameProduct = document.createElement("h5")
         nameProduct.classList.add("card-title")
         nameProduct.classList.add("text-muted")
@@ -41,12 +40,12 @@ const paintData = (data) => {
         let stockProduct = document.createElement("h2")
         stockProduct.classList.add("card-text")
         stockProduct.classList.add("text-muted")
-        stockProduct.textContent ="In Existetnce: "+" " +productos.stock
-    
+        stockProduct.textContent = "In Existetnce: " + " " + productos.stock
+
         let unit_price = document.createElement("p")
         unit_price.classList.add("card-text")
         unit_price.textContent = "$" + productos.unit_price
-    
+
         let buttonInfoProduct = document.createElement("button")
         buttonInfoProduct.setAttribute("type", "button")
         buttonInfoProduct.classList.add("btn")
@@ -54,25 +53,25 @@ const paintData = (data) => {
         buttonInfoProduct.classList.add("w-100")
         buttonInfoProduct.classList.add("shadow")
         buttonInfoProduct.textContent = "View Bag"
-    
-    
-    
-    
+
+
+
+
         //PADRES E HIJOS
         cuerpoTarjeta.appendChild(nameProduct)
         cuerpoTarjeta.appendChild(stockProduct)
         cuerpoTarjeta.appendChild(unit_price)
         cuerpoTarjeta.appendChild(buttonInfoProduct)
-    
-    
+
+
         tarjeta.appendChild(imageProduct)
         tarjeta.appendChild(divisor)
         tarjeta.appendChild(cuerpoTarjeta)
-    
+
         columna.appendChild(tarjeta)
-    
+
         fila.appendChild(columna)
-    
+
 
     });
 }
